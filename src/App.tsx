5c1fc20762d9427d1c1c1df435d4c79d04ab1cc4
Ryan G. Bitonio <ryanbitonio.ayang@gmail.com>
2023-06-13 15:09:15 +0800

    Converting traditional fetching of data to react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css';
+import "./App.css";
+import PostList from "./react-query/PostList";
 
 function App() {
-  return <h1>React Starter Project</h1>;
+  return (
+    <>
+      <PostList></PostList>
+    </>
+  );
 }
 
 export default App;
